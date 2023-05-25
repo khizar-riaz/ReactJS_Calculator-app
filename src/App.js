fe4544496d943b31ff837caa7c6c473459a4b86d
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-05-25 17:17:52 +0500

    react JS calculat update 1

--- src/App.js
+++ src/App.js
@@ -1,52 +1,38 @@
-import React, { useState } from 'react';
-import './App.css';
-
 function App() {
-  const [result, setResult] = useState('');
 
-  const handleClick = e => {
-    setResult(result + e.target.name);
-  };
+  const createDigits = () =>
+    {
+   const digits =[];
 
-  const calculate = () => {
-    try {
-      setResult(eval(result).toString());
-    } catch (error) {
-      setResult('Error');
+   for (let i = 0; i<10; i++){
+    digits.push(
+      <button key={i}>{i}</button>
+    )
+   }
+   return digits;
     }
-  };
+  return(
+  <div className="App">
+  <div className="calculator">
+  <div className="display">
+    <span>(0)</span>0
+  </div>
+  <div className="operators">
+    <button>/</button>
+    <button>*</button>
+    <button>+</button>
+    <button>-</button>
 
-  const clear = () => {
-    setResult('');
-  };
-
-  return (
-    <div className="App">
-      <h1>Calculator</h1>
-      <div className="calculator">
-        <input type="text" value={result} />
-        <div className="keypad">
-          <button name="1" onClick={handleClick}>1</button>
-          <button name="2" onClick={handleClick}>2</button>
-          <button name="3" onClick={handleClick}>3</button>
-          <button name="4" onClick={handleClick}>4</button>
-          <button name="5" onClick={handleClick}>5</button>
-          <button name="6" onClick={handleClick}>6</button>
-          <button name="7" onClick={handleClick}>7</button>
-          <button name="8" onClick={handleClick}>8</button>
-          <button name="9" onClick={handleClick}>9</button>
-          <button name="0" onClick={handleClick}>0</button>
-          <button name="+" onClick={handleClick}>+</button>
-          <button name="-" onClick={handleClick}>-</button>
-          <button name="*" onClick={handleClick}>*</button>
-          <button name="/" onClick={handleClick}>/</button>
-          <button name="." onClick={handleClick}>.</button>
-          <button onClick={clear}>Clear</button>
-          <button onClick={calculate}>=</button>
-        </div>
-      </div>
-    </div>
+    <button>DEL</button>
+  </div>
+  <div className="digits">
+    {createDigits()}
+    <button>0</button>
+    <button>.</button>
+    <button>=</button>
+  </div>
+  </div>
+  </div>
   );
 }
-
 export default App;
